a5c516f781eed5cd060780f3eaa0bc131d7ee637
twistingmercury <twistingmercury@outlook.com>
2018-11-17 13:37:32 -0800

    had to do some re-arranging of solution to get xunit to work.

---
+++ src/tsqlsh.unittests/CmdLineParserTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using tsqlsh;
+
+namespace tsqlsh.unittests
+{
+    public class CmdLineParserTests
+    {
+        [Fact]
+        public void Ctor_empty_args_should_set_ShowHelp_to_true()
+        {
+            // arrange
+            var args = new string[] { };
+
+            // act
+            var cla = new CmdLineArgs(args);
+
+            // assert
+            Assert.True(cla.ShowHelp);
+        }
+    }
+}
